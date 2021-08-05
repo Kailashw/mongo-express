82e4f0661e383fba40af3c5e4165c9528d2b2a93
Kailas Walldoddi <Kailas.Walldoddi@ellucian.com>
2021-08-05 11:58:14 +0530

    fetching creds from env file

--- mongoClient.js
+++ mongoClient.js
@@ -1,4 +1,4 @@
 const { MongoClient } = require('mongodb');
-const uri = "mongodb+srv://admin:admin@shreya.ig9nc.mongodb.net";
+const uri = process.env.MONGO_URL;
 const client = new MongoClient(uri)
 module.exports.client = client;
